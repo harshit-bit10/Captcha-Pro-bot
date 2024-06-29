4b9b7844208ce7b2a5c57fbfa1ce843e071dad94
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2024-06-29 11:37:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ import os
 
 class Config(object):
     # get it from my.telegram.org
-    APP_ID = os.environ.get("APP_ID", 123456)
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = os.environ.get("APP_ID", 27190467)
+    API_HASH = os.environ.get("API_HASH", "ff6bc6ad2faba520f426cf04ca7f5773")
     # Get it from @botfather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7442771033:AAFtr-5VeWahz-0aS5aAv3OWUDwDPUNYqNc")
     # Leave this defualt
     SESSION_NAME = os.environ.get("SESSION_NAME", "JV_CaptchaBot")
     # get it from https://cloud.mongodb.com 
-    MONGODB_URI = os.environ.get("MONGODB_URI", "")
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://raretrack907:nROK1YKWq7ytVfkz@sharktheaquaking.nqcqmbv.mongodb.net/?retryWrites=true&w=majority&appName=SharkTheAquaKing")
     # Ask in https://t.me/JV_Community
     API_TOKEN = os.environ.get("API_TOKEN", None)
     # Sudo users(goto @JVToolsBot and send /id to get your id)
-    SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS", "1204927413 1405957830").split()))
-    SUDO_USERS.append(1204927413)
+    SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS", "6066102279 7102604217").split()))
+    SUDO_USERS.append(6066102279)
     SUDO_USERS = list(set(SUDO_USERS))
